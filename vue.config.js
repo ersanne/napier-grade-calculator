315cd60ca841defc558c1f66d6b55ef7b3920158
ersanne <K1j@3QePIVFz>
2020-02-27 15:47:47 +0000

    Update config

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,6 @@ module.exports = {
     "transpileDependencies": [
         "vuetify"
     ],
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/napier-grade-calculator/'
-        : '/',
     pwa: {
         name: 'Napier Grade Calculator',
         themeColor: '#e5243f',
