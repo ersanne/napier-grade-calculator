e45d5dfeb438c86a5043c9291208ff7f694d31ff
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-25 11:59:56 +0000

    Deploy ersanne/napier-grade-calculator to github.com/ersanne/napier-grade-calculator.git:gh-pages

---
+++ precache-manifest.781e1aec11ff043283b8e11381aa64d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdd77c69739b04356a21",
+    "url": "/css/chunk-vendors.a6362231.css"
+  },
+  {
+    "revision": "257981c21769fb5b4ac3d2b7a1ca450c",
+    "url": "/img/napier_logo_white_text.257981c2.png"
+  },
+  {
+    "revision": "e6d9d7a0baca5d71cc8c3d2e130e14f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee816e1028998c9f6ce4",
+    "url": "/js/app.418e94ca.js"
+  },
+  {
+    "revision": "bdd77c69739b04356a21",
+    "url": "/js/chunk-vendors.48fe1cea.js"
+  },
+  {
+    "revision": "f0c81320bc1423a680eea17ec5338f5a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
